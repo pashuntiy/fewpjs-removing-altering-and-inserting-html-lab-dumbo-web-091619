655dd26b0884de917756cd89640f75511fbc5d3b
Pavel Ilin <pavel_ilin@yahoo.com>
2019-10-27 20:39:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
-newHeader.id = "test"
+newHeader.id = "victory"
 newHeader.innerHTML = "We is the champion!";
-newHeader.className = "test";
+newHeader.className = "victory";
