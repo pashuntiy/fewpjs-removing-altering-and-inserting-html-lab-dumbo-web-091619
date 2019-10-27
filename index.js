ae91e9cf126a3f534f0da0210d89761dc5884000
Pavel Ilin <pavel_ilin@yahoo.com>
2019-10-27 20:38:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 // Write your code here!
 
-let element = document.createElement('div')
-document.body.appendChild(element)
+document.querySelector("main#main").remove();
 
-let ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul)
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
